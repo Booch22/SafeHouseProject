552ebd4daa1dd2c79eb7264ba98f7681a28efbdd
Booch22 <jaobooch@gmail.com>
2024-05-24 13:59:36 +0700

    Update1

--- test1.py
+++ test1.py
@@ -1,26 +1,52 @@
 def bi_search( data, target ) :
     l = 0
     r = len( data ) - 1
-    m = int( (l + r) / 2 )
+    found = False
 
-    round = 0
     while l <= r :
-        print( f"round = {round}" )
+        m = ( l + r ) // 2
+        print( f"l = {l}, r = {r}, m = {m}")
 
-        m = int( (l + r) / 2 )
-        
-        if target < data[m] :
-            r = m - 1
-
-        elif target > data[m] :
+        if target > data[m] :
             l = m + 1
 
-        elif target == data[m] :
-            print( 'found target' )
-            return data[m]
-        
-        round += 1
+        elif target < data[m] :
+            r = m - 1
 
-a = [1, 3, 4, 6, 7, 11, 22, 23, 32]
-num = bi_search( a, 32 )
-print( num )
+        elif target == data[m] :
+            print( f'Found {target}' )
+            found = True
+            break
+           
+    if found == False :
+        print( f'Not found {target}' )
+
+queue = []
+
+while True :
+    print( f'Current queue {queue}' )
+    print( 'a : add\nd : delete\ns : sort\nS : search' )
+    kb = input( '=> ' )
+    
+    try :
+        if kb == 'a' :
+            num = int( input( 'add data to queue : ' ) )
+            queue.append( num )
+
+        elif kb == 'd' :
+            print( f'delete {queue[0]} from queue ' )
+            queue.pop(0)
+
+        elif kb == 's' :
+            print( f'sort queue' )
+            queue.sort()
+
+        elif kb == 'S' :
+            target = int( input('search target : ') )
+            bi_search( queue, target )
+
+    except :
+        print( 'try again' )
+
+    
+    print( '\n' )
