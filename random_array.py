447fa449ea21f824f26fa724268e4395afb0286e
Booch22 <jaobooch@gmail.com>
2024-05-24 14:35:29 +0700

    Update 1

--- random_array.py
+++ random_array.py
@@ -1,5 +1,5 @@
 import numpy as np
 import random
 
-j = np.array( [[random.randint(1, 10) for i in range(5)] for j in range(3)] )
+j = np.array( [[random.randint(1, 10) for i in range(5)] for j in range(5)] )
 print( j )
