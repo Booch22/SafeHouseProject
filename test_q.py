f046b005eef79339a0bb6d5eb4049c4b004ff51f
Booch22 <jaobooch@gmail.com>
2024-05-25 14:01:15 +0700

    25/5/2024

--- test_q.py
+++ test_q.py
@@ -1,45 +1,47 @@
 def bi_search( data, target ) :
     l = 0
-    r = len(data) - 1
+    r = len( data ) - 1
+    found = False
 
     while l <= r :
-        m = (l + r) // 2
-        
+        m = ( l + r ) // 2
+
         if target < data[m] :
             r = m - 1
 
         elif target > data[m] :
             l = m + 1
-        
+
         elif target == data[m] :
-            return data[m]
+            print( f'Found {data[m]}' )
+            found = True
+            break
 
-
-    print( 'Not found' )
-
+    if found == False :
+        print( f'Not found {target}' )
 
 queue = []
+
 while True :
     print( f'Current queue {queue}' )
     print( 'a : add\nd : delete\ns : sort\nS : search' )
     kb = input( '-> ' )
-    
+
     if kb == 'a' :
-        num = int( input('add data : ') )
+        num = int( input( 'add data in queue : ' ) )
         queue.append( num )
 
     elif kb == 'd' :
         print( f'delete {queue[0]} from queue' )
-        queue.pop(0)
+        queue.pop( 0 )
 
     elif kb == 's' :
         print( 'sort data' )
         queue.sort()
 
     elif kb == 'S' :
-        target = int( input( 'Search data -> ' ) )
-        data_search = bi_search( queue, target )
-        print( f"Found {data_search}" )
+        target = int( input( 'Search data : ' ) )
+        bi_search( queue, target )
 
 
     print( '\n' )
